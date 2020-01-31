2c82cc42f0621c521cf652dd2f072b5730c11ba3
Jano <johnrhonestquiambao@gmail.com>
2020-01-31 19:17:32 +0800

    Updates

---
+++ precache-manifest.dcd38333d0e8e8e9995af8c5a5c8ac58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbf0a9f0b14e8985564b2def3662268",
+    "url": "/react-company-website-v1/index.html"
+  },
+  {
+    "revision": "bce63ed471f2c11ff60b",
+    "url": "/react-company-website-v1/static/css/main.81d9670a.chunk.css"
+  },
+  {
+    "revision": "071b664bd3055b783c71",
+    "url": "/react-company-website-v1/static/js/2.4cee1804.chunk.js"
+  },
+  {
+    "revision": "df5b42b799525c55e9f9f7d651a296ad",
+    "url": "/react-company-website-v1/static/js/2.4cee1804.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bce63ed471f2c11ff60b",
+    "url": "/react-company-website-v1/static/js/main.b3f7b36f.chunk.js"
+  },
+  {
+    "revision": "064b69a87ca3d0b6fa81",
+    "url": "/react-company-website-v1/static/js/runtime-main.aa217be8.js"
+  }
+]);
