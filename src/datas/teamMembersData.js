3f450dd7e418b95989e77052b80615f450754253
Jano <johnrhonestquiambao@gmail.com>
2020-01-31 18:59:56 +0800

    Modified Team Members Data

--- src/datas/teamMembersData.js
+++ src/datas/teamMembersData.js
@@ -3,25 +3,25 @@ const teamMembersData = [
         id: 1,
         imgUrl: 'https://i.imgur.com/ojoSBDZ.png',
         memberName: 'Dianne Sai',
-        memberPosition: 'Position'
+        memberPosition: 'QA'
     },
     {
         id: 2,
         imgUrl: 'https://i.imgur.com/6n4pcOU.png',
         memberName: 'Oliver Smith',
-        memberPosition: 'Position'
+        memberPosition: 'Developer'
     },
     {
         id: 3,
         imgUrl: 'https://i.imgur.com/0HmbGo2.png',
         memberName: 'Jane Davids',
-        memberPosition: 'Position'
+        memberPosition: 'Designer'
     },
     {
         id: 4,
         imgUrl: 'https://i.imgur.com/kL2h3lX.png',
         memberName: 'John Collins',
-        memberPosition: 'Position'
+        memberPosition: 'Developer'
     },
 ];
 
