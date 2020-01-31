d31db45f458c63e390827c0655ae8b35a5a05f54
Jano <johnrhonestquiambao@gmail.com>
2020-01-31 20:37:34 +0800

    Updates

---
+++ precache-manifest.8efdd42f018e4d1ce10bc9fe9fe55923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86fcccd788245c08bbee930bd60b763",
+    "url": "/company-website-v1/index.html"
+  },
+  {
+    "revision": "8a5103e96aa04c6c58b3",
+    "url": "/company-website-v1/static/css/main.81d9670a.chunk.css"
+  },
+  {
+    "revision": "071b664bd3055b783c71",
+    "url": "/company-website-v1/static/js/2.4cee1804.chunk.js"
+  },
+  {
+    "revision": "df5b42b799525c55e9f9f7d651a296ad",
+    "url": "/company-website-v1/static/js/2.4cee1804.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a5103e96aa04c6c58b3",
+    "url": "/company-website-v1/static/js/main.f06f2ca0.chunk.js"
+  },
+  {
+    "revision": "00eeccbfabf91ac4f02d",
+    "url": "/company-website-v1/static/js/runtime-main.7eb187fe.js"
+  }
+]);
